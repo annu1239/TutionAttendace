b799f22b7126e0dc0077c8c3d31157c3120579f6
harjaskalsi <harjaskalsi12@gmail.com>
2020-03-18 12:50:16 +0530

    added student add

---
+++ app/src/main/java/com/uuu/tutionattendace/StudentAdd.java
@@ -0,0 +1,14 @@
+package com.uuu.tutionattendace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_add);
+    }
+}
