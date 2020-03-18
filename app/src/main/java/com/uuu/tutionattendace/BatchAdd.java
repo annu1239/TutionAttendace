edbc2383993e38b913c7ed5fa3ec43af461774d1
Annu Goel <annugoel1234@gmail.com>
2020-03-18 18:57:41 +0530

    Modified java classes

--- app/src/main/java/com/uuu/tutionattendace/BatchAdd.java
+++ app/src/main/java/com/uuu/tutionattendace/BatchAdd.java
@@ -2,12 +2,15 @@ package com.uuu.tutionattendace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -57,9 +60,33 @@ public class BatchAdd extends AppCompatActivity {
 
         @Override
         protected String doInBackground(Void... voids) {
-            return null;
+
+            String result = "";
+
+            try {
+            URL url = new URL("http://18.218.141.91/android/index.php?etName="+name+"&etClass="+cls+"&etNum="+num+"&etTeacher="+teacher+"&etTime="+time);
+
+            InputStream stream = url.openConnection().getInputStream();
+
+            InputStreamReader ir = new InputStreamReader(stream);
+
+            BufferedReader br = new BufferedReader(ir); //true false  there is a null character at end
+
+            String line = "";
+
+            while ((line = br.readLine())!= null)
+            {
+                result = result+line+"\n";// t tr tru true then loop exit when null is encountered
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+            return result;  //it is to be passed to onPostExecute
+    }
         @Override
         protected void onPreExecute() {
 
@@ -78,4 +105,42 @@ public class BatchAdd extends AppCompatActivity {
             super.onProgressUpdate(values);
         }
 
-    }}
+        @Override
+        protected void onPostExecute(String s) {  //s contains result from doInBackground
+            super.onPostExecute(s);
+
+            progressBar.setVisibility(View.INVISIBLE);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String data = s.trim();
+
+            if(data.matches("true"))
+            {
+                Toast.makeText(getApplicationContext(), "User Registeration Successful", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "User Registeration Failed", Toast.LENGTH_LONG).show();
+            }
+
+            etName.setText("");
+            etClass.setText("");
+            etNum.setText("");
+            etTeacher.setText("");
+            etTime.setText("");
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent login = new Intent(getApplicationContext(),Admin.class);
+                    startActivity(login);
+                    finish();
+                }
+            }, 1000);
+        }
+    }
+}
