6159071f141dff5f689742a1ab8306f62309419e
harjaskalsi <harjaskalsi12@gmail.com>
2020-03-29 23:29:47 +0530

    Merge branch 'master' of C:\Users\harja\AndroidStudioProjects\TutionAttendace with conflicts.

--- app/src/main/java/com/uuu/tutionattendace/MainActivity.java
+++ app/src/main/java/com/uuu/tutionattendace/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 if(mp.isPlaying())
                 {
                     mp.stop();}
-                Intent intent=new Intent(getApplicationContext(),Login.class);
+                Intent intent=new Intent(getApplicationContext(),StudentProfile.class);
                 startActivity(intent);
                 finish();//taaki back button press krne pe dubaara splash screen pe na jaaye
 
