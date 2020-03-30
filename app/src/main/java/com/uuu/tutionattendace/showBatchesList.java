d09bf0e20e186801ef007be1f99e632a3274f68f
pichimanshi <himanshi0875@gmail.com>
2020-03-30 17:45:54 +0530

    added Login pop

---
+++ app/src/main/java/com/uuu/tutionattendace/showBatchesList.java
@@ -0,0 +1,14 @@
+package com.uuu.tutionattendace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showBatchesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_batches_list);
+    }
+}
