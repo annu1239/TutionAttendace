7d2b66aefbf48290f8d05e6b0d34f002b7714029
harjaskalsi <harjaskalsi12@gmail.com>
2020-03-31 12:12:12 +0530

    jaisu changes

---
+++ app/src/main/java/com/uuu/tutionattendace/Teacher2stu.java
@@ -0,0 +1,14 @@
+package com.uuu.tutionattendace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Teacher2stu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher2stu);
+    }
+}
