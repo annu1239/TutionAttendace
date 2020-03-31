ee97cef27551bbada479ffdb17ee322f123ef019
Annu Goel <annugoel1234@gmail.com>
2020-03-31 12:51:59 +0530

    Modified BatchListFragment nd StudentBT and FragmentInfo java nd xml files

--- app/src/main/java/com/uuu/tutionattendace/BatchListFragment.java
+++ app/src/main/java/com/uuu/tutionattendace/BatchListFragment.java
@@ -7,12 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BatchListFragment extends Fragment {
+
+    Button btnDetails, btnAttendance;
 
     public BatchListFragment() {
         // Required empty public constructor
@@ -23,6 +26,8 @@ public class BatchListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_batch_list, container, false);
     }
+
 }
--- app/src/main/java/com/uuu/tutionattendace/StudentBT.java
+++ app/src/main/java/com/uuu/tutionattendace/StudentBT.java
@@ -3,12 +3,22 @@ package com.uuu.tutionattendace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StudentBT extends AppCompatActivity {
+
+    TextView tvName, tvContact, tvAddress, tvAttendance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_b_t);
+
+        tvName = findViewById(R.id.tvName);
+        tvContact = findViewById(R.id.tvContact);
+        tvAddress = findViewById(R.id.tvAddress);
+        tvAttendance = findViewById(R.id.tvAttendance);
+
+
     }
 }
